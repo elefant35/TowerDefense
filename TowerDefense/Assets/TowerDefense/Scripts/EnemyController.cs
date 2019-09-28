f969e7a2447132aa8c74d1cb1ab0c205a2eefe9e
Tyler Coleman <tyler.coleman@gmail.com>
2019-09-27 21:52:47 -0700

    Created Unity project
    And set up navmesh, a basic maze and placeholder Materials

---
+++ TowerDefense/Assets/TowerDefense/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    //Selectable variables
+    public GameObject Goal;
+    public NavMeshAgent EnemyNavMesh;
+    //Non-Selectable variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();
+    }
+    
+    void EnemyMovement()
+    {
+        EnemyNavMesh.SetDestination(Goal.transform.position);
+    }
+
+}
