b0ae6360ea9fb99964dd1b2a59bdc174883840d9
Tyler Coleman <tyler.coleman@gmail.com>
2019-10-12 17:57:38 -0700

    Added ability to change score

---
+++ TowerDefense/Assets/TowerDefense/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //declare variables
+    public int score = 0;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeScore(int scoreChange) //adds to score (or subtracts with negative input)
+    {
+        score += scoreChange;
+        scoreText.text = score.ToString();
+    }
+}
