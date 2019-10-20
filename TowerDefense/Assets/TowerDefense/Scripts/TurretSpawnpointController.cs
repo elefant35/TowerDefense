eddad7498ef6d500b9df59611958076380b8ccff
Tyler Coleman <tyler.coleman@gmail.com>
2019-10-20 12:26:44 -0700

    Claened up Turret Code
    Cleaned up the turret code to remove public variables and add needed public functions. Added layers of abstraction

--- TowerDefense/Assets/TowerDefense/Scripts/TurretSpawnpointController.cs
+++ TowerDefense/Assets/TowerDefense/Scripts/TurretSpawnpointController.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class TurretSpawnpointController : MonoBehaviour
 {
     //declare variables
-    public Transform spawnPoint;
-    public GameObject turret;
-    public GameObject turretToSpawn;
+    [SerializeField] private Transform spawnPoint; // The point where the turret will spawn
+    [SerializeField] private GameObject[] turretLibrary; // using an array so I can add additional turrets in the future
+    private GameObject currentTurret; // keeps track of current turret in case modifations to turret need to be made, eg. upgrades. 
+    private bool hasTurret = false; //shows if this turret spawn point currently has a turret 
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnTurret();
+        ChangeTurret(0);// used for testing remove later
     }
 
     // Update is called once per frame
@@ -21,9 +22,49 @@ public class TurretSpawnpointController : MonoBehaviour
         
     }
 
-    public void spawnTurret()
+    private GameObject spawnTurret( GameObject turretToSpawn)
     {
-        turret = (GameObject)Instantiate(turretToSpawn, spawnPoint.position, spawnPoint.rotation);
-        Debug.Log("Turret was spawned");
+        Debug.Log(turretToSpawn.name + "was spawned"); //logs what turret was spawned
+        hasTurret = true; //sets hasTurret to true to show that the spawnpoint has a turret
+        return (GameObject)Instantiate(turretToSpawn, spawnPoint.position, spawnPoint.rotation); //spawns turret and 
+        
     }
+
+    public GameObject returnCurrentTurret() // used to return the current turret so outside scripts can  see what turret is being used without modifying it
+    {
+        return currentTurret;
+    }
+
+    public bool removeTurret()
+    {
+        if (hasTurret)
+        {
+            //remove turret
+            hasTurret = false; //sets has turret to false to show no turret is in use
+            Destroy(currentTurret); //destroys the turret gameobject
+            currentTurret = null; //sets currentTurret to null to reperesent that no turret is in use
+            return true; //returns true for error handling to show that the function was sucessful
+        }
+        else
+        {
+            return false; // there is currently no turret so false is returned for error handling
+        }
+    }
+
+    public bool returnHasTurret()
+    {
+        return hasTurret;
+    }
+    
+    public bool ChangeTurret(int turretNumber) // adds a turret or changes it regardless of existing turret in spawnpoint
+    {
+        if (hasTurret)
+        {
+            removeTurret(); // if there is a turret then remove it
+        }
+        currentTurret = spawnTurret(turretLibrary[turretNumber]); //spawn a turret
+        return false;
+    }
+
+
 }
